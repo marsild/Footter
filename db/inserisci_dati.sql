51c121f9ef79fddbd6f2e1fa654a302782da4394
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-01-20 15:32:48 +0100

    aggiunta query inserimento

--- db/inserisci_dati.sql
+++ db/inserisci_dati.sql
@@ -1,3 +1,28 @@
+INSERT INTO `squadra` (`nome`, `logo`) VALUES
+('Atalanta', 'atalanta.png'),
+('Bologna', 'bologna.png'),
+('Cremonese', 'cremonese.png'),
+('Empoli', 'empoli.png'),
+('Fiorentina', 'fiorentina.png'),
+('Hellas', 'hellas.png'),
+('Inter', 'inter.png'),
+('Juventus', 'juventus.png'),
+('Lazio', 'lazio.png'),
+('Lecce', 'lecce.png'),
+('Milan', 'milan.png'),
+('Monza', 'monza.png'),
+('Napoli', 'napoli.png'),
+('Roma', 'roma.png'),
+('Salernitana', 'salernitana.png'),
+('Sampdoria', 'sampdoria.png'),
+('Sassuolo', 'sassuolo.png'),
+('Spezia', 'spezia.png'),
+('Torino', 'torino.png'),
+('Udinese', 'udinese.png');
+
+INSERT INTO `riguarda` (`squadra`, `id_post`) VALUES
+('Milan', 1);
+
 INSERT INTO `utente` (`nome`, `cognome`, `psw`, `email`, `immagine`, `n_follower`, `n_seguiti`, `nickname`) VALUES
 ('Marsild', 'Spahiu', '12345678', 'marsildspahiu@gmail.com', 'test', 0, 0, 'marsild');
 
