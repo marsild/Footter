7b7e9321048feb6fe41066cac05264d9d7e882f4
Luca <luca.pasini8@studio.unibo.it>
2023-01-22 16:04:58 +0100

    put isset condition

--- template/aside_personalizza.php
+++ template/aside_personalizza.php
@@ -1,13 +1,20 @@
 <h6 class="text-center text-white">Filtra per</h6>
 <div>
 <?php
-    if($_GET["filtra"] == "preferiti"){
-        echo '<a href="index.php?filtra=tutti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Tutti <em class="bi bi-check-circle"></em></a>';
-        echo '<a href="index.php?filtra=preferiti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Preferiti <em class="bi bi-check-circle-fill text-info"></em></a>';
-    } else {
+    if(isset($_GET["filtra"])){
+        if ($_GET["filtra"] == "preferiti"){
+            echo '<a href="index.php?filtra=tutti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Tutti <em class="bi bi-check-circle"></em></a>';
+            echo '<a href="index.php?filtra=preferiti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Preferiti <em class="bi bi-check-circle-fill text-info"></em></a>';
+        } else {
+            echo '<a href="index.php?filtra=tutti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Tutti <em class="bi bi-check-circle-fill text-info"></em></a>';
+            echo '<a href="index.php?filtra=preferiti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Preferiti <em class="bi bi-check-circle"></em></a>';
+        }
+    }
+    else {
         echo '<a href="index.php?filtra=tutti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Tutti <em class="bi bi-check-circle-fill text-info"></em></a>';
         echo '<a href="index.php?filtra=preferiti" role="button" class="btn btn-link text-decoration-none text-white w-100 text-start px-0">Preferiti <em class="bi bi-check-circle"></em></a>';
     }
+    
 ?>
 
 
