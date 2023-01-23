aa9cccfdf8e1cce26747891c3c42c05937ed4046
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-01-23 23:37:12 +0100

    tags and action/method in form

--- template/form_registrati1.php
+++ template/form_registrati1.php
@@ -1,49 +1,41 @@
-<form style="width: 22rem;">
+<form style="width: 22rem;" action="registrati2.php" method="POST">
     <!-- Nome Cognome input -->
     <div class="mb-4 row g-3">
         <div class="col">
-            <input type="text" class="form-control" placeholder="Nome" aria-label="Nome">
+            <input type="text" class="form-control" id="reg_nome" placeholder="Nome" aria-label="Nome">
         </div>
         <div class="col">
-            <input type="text" class="form-control" placeholder="Cognome" aria-label="Cognome">
+            <input type="text" class="form-control" id="reg_cognome" placeholder="Cognome" aria-label="Cognome">
         </div>
     </div>
     <!-- Email input -->
     <div class="form-outline mb-4">
-        <input type="password" id="form2Example3" class="form-control" placeholder="Email">
-        <label class="form-label" for="form2Example3" style="margin-left: 0px;" hidden>Email</label>
+        <input type="text" id="reg_email" class="form-control" placeholder="Email" required>
+        <label class="form-label" for="reg_email" style="margin-left: 0px;" hidden>Email</label>
         <div class="form-notch">
             <div class="form-notch-leading" style="width: 9px;"></div>
             <div class="form-notch-middle" style="width: 64px;"></div>
             <div class="form-notch-trailing"></div>
         </div>
     </div>
-
+    <!-- Username input -->
     <div class="input-group mb-4">
         <span class="input-group-text" id="basic-addon1">@</span>
-        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <input type="text" class="form-control" id="reg_username" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required>
     </div>
     <!-- Password input -->
     <div class="form-outline mb-4">
-        <input type="password" id="form2Example2" class="form-control" placeholder="Password">
-        <label class="form-label" for="form2Example2" style="margin-left: 0px;" hidden>Password</label>
+        <input type="password" id="reg_password" class="form-control" placeholder="Password" required>
+        <label class="form-label" for="reg_password" style="margin-left: 0px;" hidden>Password</label>
         <div class="form-notch">
             <div class="form-notch-leading" style="width: 9px;"></div>
             <div class="form-notch-middle" style="width: 64px;"></div>
             <div class="form-notch-trailing"></div>
-        </div>
-    </div>
-
-    <!-- 2 column grid layout for inline styling -->
-    <div class="row mb-4">
-        <div class="col d-flex justify-content-start">
-            <!-- Checkbox -->
-
         </div>
     </div>
 
     <!-- Continua button -->
     <div class="d-grid col-5">
-        <a href="registrati2.php" role="button" class="btn btn-primary btn-block w-20">Continua</a>
+        <input type="submit" value="Continua" class="btn btn-primary btn-block w-20">
     </div>
 </form>
