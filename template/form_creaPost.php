9959eb2499911604704cce22999191461f921df9
Luca <luca.pasini8@studio.unibo.it>
2023-01-25 18:39:33 +0100

    update creaPost front end

--- template/form_creaPost.php
+++ template/form_creaPost.php
@@ -1,38 +1,41 @@
-<div class="mx-3">
-    <form style="width: 22rem" action="crea.php" method="POST">
-
-        <label for="textarea" class="form-label"></label>
-        <textarea class="form-control " maxlength="255" name="textarea" id="textarea" placeholder="Inserisci il testo.." rows="3"></textarea>
-
-
-        <div class="d-grid gap-2 col-8 mt-2">
-
-            <input type="file" name="imgpost" id="imgpost" value="Inserisci un'immagine..">
-
-
+<form action="crea.php" method="POST">
+    <div class="row mx-3 mb-4 text-center">
+        <div class="col-12  ">
+            <label for="textarea" class="form-label"></label>
+            <textarea class="form-control " maxlength="255" name="textarea" id="textarea" placeholder="Inserisci il testo.." rows="3"></textarea>
+        </div>
+    </div>
+    <div class="row mx-3 mb-4">
+        <div class="col-12 ">
+            <input type="file" class=" form-control btn btn-outline-dark" name="imgpost" id="imgpost" value="Inserisci un'immagine..">
+        </div>
+    </div>
+    <div class="row mx-3">
+        <div class="col-12 ">
             <select class="form-select form-select-sm" name="first-squad" aria-label="Default select example" required>
                 <option selected disabled value="">Inserisci una squadra</option>
                 <?php foreach ($templateParams["squadre"] as $squadra) : ?>
                     <option value=<?php echo $squadra["nome"] ?>><?php echo $squadra["nome"];  ?></option>
                 <?php endforeach ?>
             </select>
-
-
+        </div>
+    </div>
+    <div class="row mx-3 mb-5">
+        <div class="col-12 ">
             <select class="form-select form-select-sm" name="second-squad" aria-label="Default select example">
                 <option selected disabled value="">Inserisci un'altra squadra</option>
                 <?php foreach ($templateParams["squadre"] as $squadra) : ?>
                     <option value=<?php echo $squadra["nome"] ?>><?php echo $squadra["nome"];  ?></option>
                 <?php endforeach ?>
             </select>
-
-
         </div>
-
-        <div class="w-100  mt-3">
-            <input class="btn btn-info float-end" type="submit" value="Pubblica">
+    </div>
+    <div class="row mx-3">
+        <div class="col text-center">
             <a href="index.php">Annulla</a>
         </div>
-
-
-    </form>
-</div>
+        <div class="col text-center">
+            <input class="btn btn-info" type="submit" value="Pubblica">
+        </div>
+    </div>
+</form>
