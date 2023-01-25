842e155449586736309d927a891f249f0dcdf7e2
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-01-25 09:39:55 +0100

    create profilo and manage follow/unfollow

---
+++ profilo.php
@@ -0,0 +1,57 @@
+<?php
+ 
+require_once("bootstrap.php");
+    /*Se è stato premuto il pulsante Segui oppure Smetti di seguire*/
+    if(isset($_POST["un_follow"])){
+        if($_POST["un_follow"]=="Segui"){
+            //INSERT in segue
+            $dbh->insertSegue($_SESSION["username"], $_GET["usr"]);
+            //UPDATE incrementa n_seguiti in profilo 1 e 2 n_follower in profilo
+            $dbh->increaseFollowerSeguiti($_SESSION["username"], $_GET["usr"]);
+            /* NOTIFICA */
+        }
+        if($_POST["un_follow"]=="Smetti di seguire"){
+            //DELETE from segue
+            $dbh->deleteSegue($_SESSION["username"], $_GET["usr"]);
+            //UPDATE decrementa n_seguiti in profilo 1 n_seguiti e n_follower in profilo 2
+            $dbh->decreaseFollowerSeguiti($_SESSION["username"], $_GET["usr"]);
+        }
+    }
+    /*Se l'username non è quello del profilo che ha effettuato il login*/
+    if(isset($_GET["usr"]) &&  $_GET["usr"] != $_SESSION["username"]){
+        /* Ottieni informatzioni utente */
+        $templateParams["profilo"]=$dbh->getUtente($_GET["usr"])[0];
+        /* Ottieni numero post utente, se non ne ha poni = 0 */
+        if($dbh->getNrOfPost($_GET["usr"]) != NULL ){
+            $templateParams["n_post"]=$dbh->getNrOfPost($_GET["usr"])[0];
+        } else {
+            $templateParams["n_post"]= 0;
+        }
+        /* Se l'utente è già seguito, metti testo nel pulsante "Smetti di seguire", se no "segui" */
+        if (in_array($_GET["usr"], flatArray($dbh->getSeguiti($_SESSION["username"]), "nickname_seguito"))){
+            $templateParams["testoPulsante"]= "Smetti di seguire";
+        } else {
+            $templateParams["testoPulsante"]= "Segui";
+        }
+        /* il pulsante riporta alla stessa pagina con lo stesso get */
+        $templateParams["actionPulsante"]="profilo.php?usr=".$templateParams["profilo"]["nickname"];
+        $templateParams["active"]="Profilo - ".$_GET["usr"];
+    } else {
+        /*Se è il profilo dell'utente loggato*/
+        $templateParams["profilo"]=$dbh->getUtente($_SESSION["username"])[0];
+        if($dbh->getNrOfPost($_SESSION["username"]) != NULL){
+            $templateParams["n_post"]=$dbh->getNrOfPost($_SESSION["username"])[0];
+        } else {
+            $templateParams["n_post"]= 0;
+        }
+        $templateParams["testoPulsante"]="Modifica";
+        $templateParams["actionPulsante"]="#";
+        $templateParams["active"]="Profilo";
+    }
+    
+    $templateParams["nome"]="main_profilo.php";
+
+    require("template/base.php");
+
+
+?>
---
+++ template/main_profilo.php
@@ -0,0 +1,25 @@
+<div class="row mx-2">
+    <div class="col-4 my-auto text-center my-3">
+        <img class="rounded-circle border border-1" style="max-height: 80px;" src="<?php echo UPLOAD_DIR.$templateParams["profilo"]["immagine"] ;?>" alt="Immagine profilo">
+    </div>
+    <div class="col-8 my-3">
+        <p><?php echo $templateParams["profilo"]["nome"] ;?> <?php echo $templateParams["profilo"]["cognome"] ;?> <br /> @<?php echo $templateParams["profilo"]["nickname"] ;?> </p>
+        <form action="<?php echo $templateParams["actionPulsante"];?>" method="POST">
+        <input type="hidden" id="un_follow" name="un_follow" value="<?php echo $templateParams["testoPulsante"] ;?>">
+        <input type="submit" value="<?php echo $templateParams["testoPulsante"] ;?>" class="btn btn-light border">
+        </form>
+    </div>
+</div>
+<hr class="text-dark mt-0">
+<div class="row mx-2 g-1">
+    <div class="col text-center">
+        <p><?php echo $templateParams["profilo"]["n_follower"] ;?> <br /> follower </p>
+    </div>
+    <div class="col text-center">
+        <p><?php echo $templateParams["profilo"]["n_seguiti"] ;?> <br /> seguiti </p>
+    </div>
+    <div class="col text-center">
+        <p><?php echo $templateParams["n_post"]["total"];?> <br /> post </p>
+    </div>
+</div>
+<hr class="text-dark my-0">
