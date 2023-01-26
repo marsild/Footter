15dcaa069d1bc3cdb0f16cec303bb58368a9c833
Luca <luca.pasini8@studio.unibo.it>
2023-01-26 11:01:31 +0100

    insert post fix

--- crea.php
+++ crea.php
@@ -2,21 +2,21 @@
 
 require_once("bootstrap.php");
 
-if(isset($_POST["textarea"])){
-    $id_post = $dbh->insertPost($_POST["imgpost"],$_POST["textarea"],$_SESSION["username"]);
-    if($_POST["first-squad"]!=$_POST["second-squad"]){
-        $dbh->insertSquadsPost($id_post,$_POST["first-squad"],$_POST["second-squad"]);
-    }else{
-        $dbh->insertSquadsPost($id_post,$_POST["first-squad"],null);
+if (isset($_POST["textarea"])) {
+    $id_post = $dbh->insertPost($_POST["imgpost"], $_POST["textarea"], $_SESSION["username"]);
+    if (isset($_POST["second-squad"])) {
+        if ($_POST["first-squad"] != $_POST["second-squad"]) {
+            $dbh->insertSquadsPost($id_post, $_POST["first-squad"], $_POST["second-squad"]);
+        } else {
+            $dbh->insertSquadsPost($id_post, $_POST["first-squad"], null);
+        }
+    } else {
+        $dbh->insertSquadsPost($id_post, $_POST["first-squad"], null);
     }
 
+    $templateParams["creato"] = "post creato con successo";
 }
-$templateParams["active"]="Crea";
-$templateParams["nome"]="form_creaPost.php";
-$templateParams["squadre"]=$dbh->getSquads();
+$templateParams["active"] = "Crea";
+$templateParams["nome"] = "form_creaPost.php";
+$templateParams["squadre"] = $dbh->getSquads();
 require("template/base.php");
-    
-
-
-
-?>
--- template/form_creaPost.php
+++ template/form_creaPost.php
@@ -1,7 +1,26 @@
 <form action="crea.php" method="POST">
+    <?php if (isset($templateParams["creato"])) { ?>
+        <div class="row mx-3">
+            <div class="col-12">
+                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                    </symbol>
+                </svg>
+                <div class="alert alert-success d-flex align-items-center mt-4 mb-0" role="alert">
+                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+                        <use xlink:href="#check-circle-fill" />
+                    </svg>
+                    <div>
+                        <?php echo $templateParams["creato"]; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
     <div class="row mx-3 mb-4 text-center">
-        <div class="col-12  ">
-            <label for="textarea" class="form-label"></label>
+        <div class="col-12 mt-4 ">
+            <label for="textarea" class="form-label" hidden>Testo post:</label>
             <textarea class="form-control " maxlength="255" name="textarea" id="textarea" placeholder="Inserisci il testo.." rows="3"></textarea>
         </div>
     </div>
