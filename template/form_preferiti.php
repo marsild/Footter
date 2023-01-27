a4b00c1d1fad7b11a86912d1a73e1a04cbf0ade8
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-01-27 20:29:53 +0100

    text pos in form_preferiti

--- template/form_preferiti.php
+++ template/form_preferiti.php
@@ -1,19 +1,25 @@
-<div style="font-size:medium;" class="text-center mt-4 mb-3 col-12"> Aggiorna le tue squadre preferite </div>
-<form class="mx-lg-5" action="processa-inserimento.php" method="POST">
+<form class="mx-md-5" action="processa-inserimento.php" method="POST">
+    <div class="row mx-3">
+        <div class="col-12 mt-4 mb-3 px-0 text-center text-md-start">
+            Aggiorna le tue squadre preferite
+        </div>
+    </div>
     <input type="hidden" id="aggiorna_preferiti" name="aggiorna_preferiti" value="on">
 
     <div class="row col-12 mx-3">
-    <?php foreach($dbh->getSquads() as $squadra):?>
-        <div class="form-check form-check-inline mx-0 my-1 col-6 ">
-            <input class="form-check-input" type="checkbox" value="<?php echo $squadra["nome"];?>" id="box<?php echo $squadra["nome"];?>" name="box<?php echo $squadra["nome"];?>" <?php if (in_array($squadra["nome"], flatArray($templateParams["preferiti"], "squadra"))) {echo "checked"; } ?>>
-            <label style="font-size:small" class="form-check-label" for="box<?php echo $squadra["nome"];?>">
-                <img src="<?php echo UPLOAD_DIR.$squadra["logo"];?>" alt="<?php echo $squadra["nome"];?>" style="max-height: 20px; ">
-                <?php echo $squadra["nome"];?>
-            </label>
-        </div>
-    <?php endforeach; ?>
+        <?php foreach ($dbh->getSquads() as $squadra) : ?>
+            <div class="form-check form-check-inline mx-0 my-1 col-6 ">
+                <input class="form-check-input" type="checkbox" value="<?php echo $squadra["nome"]; ?>" id="box<?php echo $squadra["nome"]; ?>" name="box<?php echo $squadra["nome"]; ?>" <?php if (in_array($squadra["nome"], flatArray($templateParams["preferiti"], "squadra"))) {
+                                                                                                                                                                                            echo "checked";
+                                                                                                                                                                                        } ?>>
+                <label style="font-size:small" class="form-check-label" for="box<?php echo $squadra["nome"]; ?>">
+                    <img src="<?php echo UPLOAD_DIR . $squadra["logo"]; ?>" alt="<?php echo $squadra["nome"]; ?>" style="max-height: 20px; ">
+                    <?php echo $squadra["nome"]; ?>
+                </label>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <?php if (isset($_GET["result"]) && $_GET["result"]=="successful") { ?>
+    <?php if (isset($_GET["result"]) && $_GET["result"] == "successful") { ?>
         <div class="row mx-3">
             <div class="col-12 px-0">
                 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -35,7 +41,7 @@
     <!-- Submit button -->
     <div class="row mx-3">
         <div class="col-12 mt-3 px-0">
-        <input type="submit" value="Aggiorna" class="btn btn-primary">
+            <input type="submit" value="Aggiorna" class="btn btn-primary">
         </div>
     </div>
 </form>
