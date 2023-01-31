017c6445466563a29b62dec586880906f07323d1
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-01-31 11:27:08 +0100

    differentiate ids in profilo

--- template/main_profilo.php
+++ template/main_profilo.php
@@ -147,11 +147,11 @@
                                         </div>
                                     </div>
                                     <!-- Modal Elimina -->
-                                    <div class="modal fade" id="confirmModal<?php echo $post["id"]; ?>" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+                                    <div class="modal fade" id="confirmModal<?php echo $post["id"]; ?>" tabindex="-1" aria-labelledby="confirmModalLabel<?php echo $post["id"]; ?>" aria-hidden="true">
                                         <div class="modal-dialog modal-dialog-centered">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="confirmModalLabel">Eliminazione post</h5>
+                                                    <h5 class="modal-title" id="confirmModalLabel<?php echo $post["id"]; ?>">Eliminazione post</h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
@@ -160,7 +160,7 @@
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
                                                     <form action="profilo.php" method="POST">
-                                                        <input type="hidden" id="eliminazione_post" name="eliminazione_post" value="<?php echo $post["id"]; ?>">
+                                                        <input type="hidden" id="eliminazione_post<?php echo $post["id"]; ?>" name="eliminazione_post" value="<?php echo $post["id"]; ?>">
                                                         <input type="submit" value="Conferma" class="btn btn-primary">
                                                     </form>
                                                 </div>
@@ -168,11 +168,11 @@
                                         </div>
                                     </div>
                                     <!-- Modal elenco like-->
-                                    <div class="modal fade" id="likeModal<?php echo $post["id"]; ?>" tabindex="-1" aria-labelledby="likeModalLabel" aria-hidden="true">
+                                    <div class="modal fade" id="likeModal<?php echo $post["id"]; ?>" tabindex="-1" aria-labelledby="likeModalLabel<?php echo $post["id"]; ?>" aria-hidden="true">
                                         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="likeModalLabel">Likes</h5>
+                                                    <h5 class="modal-title" id="likeModalLabel<?php echo $post["id"]; ?>">Likes</h5>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
