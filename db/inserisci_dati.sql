9b7ce52b1c7d127b96bcbf95fbb60de5237ba327
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2023-02-13 22:52:55 +0100

    remove useless insert queries

--- db/inserisci_dati.sql
+++ db/inserisci_dati.sql
@@ -19,22 +19,3 @@ INSERT INTO `squadra` (`nome`, `logo`) VALUES
 ('Spezia', 'spezia.png'),
 ('Torino', 'torino.png'),
 ('Udinese', 'udinese.png');
-
-INSERT INTO `utente` (`nome`, `cognome`, `psw`, `email`, `immagine`, `n_follower`, `n_seguiti`, `nickname`) VALUES
-('Marsild', 'Spahiu', '12345678', 'marsildspahiu@gmail.com', 'pfp.png', 0, 0, 'marsild');
-
-INSERT INTO `post` (`id`, `immagine`, `testo`, `n_like`, `n_commenti`, `data_post`, `nickname`) VALUES
-(1, 'pfp.png', 'Primo post', 0, 0, '2022-12-29', 'marsild');
-
-INSERT INTO `riguarda` (`squadra`, `id_post`) VALUES
-('Milan', 1),
-('Sassuolo', 1);
-
-INSERT INTO `commento` (`data_commento`,`testo`,`nickname`,`id_post`) VALUES
-    ('2022-12-29','wmoveinvwoinvoienvo','marsild',1);
-
-INSERT INTO `segue` (`nickname_segue`, `nickname_seguito`) VALUES
-('lucapaso', 'marsild');
-
-INSERT INTO `preferiti` (`squadra`, `nickname`) VALUES
-('Sassuolo', 'lucapaso');
